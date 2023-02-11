806a2ae7f27376bc77942ebf09dc688f55d51d57
WassCodeur <wachioubouraima56@gmail.com>
2023-02-11 16:18:29 +0000

    creating the class FileStorage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""storage module"""
+
+
+class FileStorage:
+    """FileStorage class"""
+    def __init(self):
+        """__init__ method"""
+        pass
+
+    def reload(self):
+        """method reload"""
+        pass
